8277953b5164930602170a4ee123dd10cbd65f48
Ahmed Seleem <ahmedalaaseleem989@gmail.com>
2026-07-28 19:22:21 +0300

    Add favicon using Brilliant Academy logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "أكاديمية بريلينت | Brilliant Academy",
   description: "منصة تعليمية أونلاين لطلاب مناهج دول الخليج - حصص فردية مع مدرسين متخصصين",
+  icons: {
+    icon: "https://assets.cdn.filesafe.space/lTNn7BkMGcm52L4pwJS0/media/69afadb7c509a0bfb75719bb.png",
+  },
 };
 
 export default function RootLayout({
