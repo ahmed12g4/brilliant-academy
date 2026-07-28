f2179d51881e01a2f5b89757a72d57df8d5b2d7d
Ahmed Seleem <ahmedalaaseleem989@gmail.com>
2026-07-28 19:46:10 +0300

    Add favicon as local file for better browser support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: "أكاديمية بريلينت | Brilliant Academy",
   description: "منصة تعليمية أونلاين لطلاب مناهج دول الخليج - حصص فردية مع مدرسين متخصصين",
   icons: {
-    icon: "https://assets.cdn.filesafe.space/lTNn7BkMGcm52L4pwJS0/media/69afadb7c509a0bfb75719bb.png",
+    icon: "/favicon.png",
   },
 };
 
